b89bfdcd21599538dfdf765e3fb49dce4ecc5ca3
marc-antoine jean <jean.marcantoine01@gmail.com>
2024-06-27 22:42:37 +0200

    add files

--- nakama_dependency_list.py
+++ nakama_dependency_list.py
@@ -1,1 +1,2 @@
-pacs = ["mysql-connector-python", "pushbullet.py"]
+packages = [["mysql-connector-python", "8.4.0"], ["pushbullet.py", "0.12.0"]]
+libraries = ["DFRobot_sensors", "hd44780", "in_house"]
